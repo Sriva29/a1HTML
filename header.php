4d95b35490940f44bdea32759907ad32008c3289
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2023-11-10 17:10:50 -0500

    css progress

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <img src="img/moustache.png" width="100" height="50" alt="A logo of a moustache">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
